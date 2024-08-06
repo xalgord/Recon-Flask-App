2023ffe29a944f50d74e0316f78fb18886de7eae
Krishna Kumar <xalgord@gmail.com>
2024-08-06 21:08:29 +0530

    Update and rename auto3.sh to auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # File containing list of domains
-domains_file=$(sort -R $1)
+domains_file=$(sort -R "$1")
 
 # Colors
 RED='\033[0;31m'
@@ -9,6 +9,10 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
+
+# Signal trapping
+trap "echo -e '${RED}Script interrupted. Exiting...${NC}'; exit 1" SIGINT SIGTERM
+trap "echo -e '${RED}Script stopped. Exiting...${NC}'; exit 1" SIGTSTP
 
 # Check if required tools are installed
 required_tools=("assetfinder" "curl" "jq" "subfinder" "findomain" "httpx" "anew" "gf" "uro" "kxss" "naabu" "nf" "nuclei" "gau" "katana" "notify" "python3")
@@ -36,11 +40,7 @@ do
 
     # Run subdomain enumeration tools
     assetfinder -subs-only "$domain" | grep "$domain" > assetfinder.txt || echo -e "${RED}assetfinder timed out or failed${NC}"
-
-	crtsh -d "$domain" --recursive | sort -u > crt.txt || echo -e "${RED}crt.sh timed out or failed${NC}"
-
     subfinder -d "$domain" -all -recursive -t 200 -silent > subfinder-recursive.txt || echo -e "${RED}subfinder timed out or failed${NC}"
-
     findomain -t "$domain" -q > findomain.txt || echo -e "${RED}findomain timed out or failed${NC}"
 
     # Merge all subdomains into all.txt
@@ -56,7 +56,7 @@ echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Running HTTPX on subs.txt...${NC}"
 echo -e "${BLUE}-----------------------------------------${NC}"
 
-httpx -t 100 -sc -title -cl -probe -l subs.txt | grep -v "FAILED" > live.txt
+httpx -t 150 -sc -title -cl -probe -l subs.txt | grep -v "FAILED" > live.txt
 cat live.txt | anew juicy-live.txt | sed 's|^[^/]*//||' | cut -d '/' -f 1 | cut -d " " -f 1 > new.txt
 
 # Extracting important subdomains
@@ -72,13 +72,12 @@ python3 ~/tools/spyhunt/spyhunt.py -isubs new.txt
 # echo -e "${BLUE}-----------------------------------------${NC}"
 # nf -d "$domain"
 
-
 # Running Nuclei
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Running Nuclei scan...${NC}"
 echo -e "${BLUE}-----------------------------------------${NC}"
 
-nuclei -l new.txt -et ssl -nh -nmhe -nss -es info,low -headless -o vulns.txt
+nuclei -l new.txt -nh -nmhe -nss -es info,low -headless -o vulns.txt
 cat vulns.txt | notify
 
 # Running FFUF
@@ -89,17 +88,17 @@ echo -e "${BLUE}-----------------------------------------${NC}"
 # for subdomain in $(cat new.txt)
 # do
 # 	ffuf -c -ac -w /root/lists/dirsearch.txt -mc 200 -rate 100 -recursion -r -u "https://$subdomain/FUZZ" >> "$ffuf_dir/result.txt"
-# done	
+# done
 
-dirsearch -l juice_subs.txt --config ~/.config/dirsearch/config.ini -t 75 --skip-on-status 429 > "dirsearch.txt"
+dirsearch -l juice_subs.txt --config ~/.config/dirsearch/config.ini -t 100 --skip-on-status 429 > "dirsearch.txt"
 
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Checking for XSS...${NC}"
 echo -e "${BLUE}-----------------------------------------${NC}"
 
 while IFS= read -r domain; do
-	python3 "/root/ParamSpider/paramspider.py" -l "$domain" --exclude "$excluded_extensions" --level high --quiet -o "${content_dir}/params.txt"
-	cat "${content_dir}/params.txt" >> "${content_dir}/paramsall.txt"
+    python3 "/root/ParamSpider/paramspider.py" -l "$domain" --exclude "$excluded_extensions" --level high --quiet -o "${content_dir}/params.txt"
+    cat "${content_dir}/params.txt" >> "${content_dir}/paramsall.txt"
 done < "$domains_file"
 
 echo "$domains_file" | katana -passive -pss waybackarchive,commoncrawl,alienvault -f qurl > "${content_dir}/katana.txt"
@@ -108,8 +107,6 @@ cat "${content_dir}/"*.txt | uro > endpoints.txt
 
 cat endpoints.txt | gf xss | uro | kxss | grep "\" ' <" | tee possible-xss.txt | notify
 
-
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${YELLOW}Reconnaissance completed${NC}"
 echo -e "${BLUE}-----------------------------------------${NC}"
-
